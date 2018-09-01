f634dd9f9e154fb865c5d0b071a1158d193fc4a7
Akash Desai <kash650@gmail.com>
2018-08-31 17:04:34 -0700

    BI-33 unit test for core data concurrent adds and erase all

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -17,17 +17,24 @@ class Tests: XCTestCase {
         let coreDataManager = CoreDataManager()
         let desiredCount = 5
 
-//        DispatchQueue.concurrentPerform(iterations: desiredCount) { iteration in
-        for iteration in 1...desiredCount {
+        let group = DispatchGroup()
+        for _ in 1...desiredCount { group.enter() }
+        DispatchQueue.concurrentPerform(iterations: desiredCount) { iteration in
+//        for iteration in 1...desiredCount {
             switch iteration % 2 {
             case 0:
                 coreDataManager.addEvent(for: "appOpen")
             default:
                 coreDataManager.addEvent(for: "appClose")
             }
+            group.leave()
         }
 
-        XCTAssert(coreDataManager.eventsCount() == desiredCount)
+        XCTAssert(group.wait(timeout: .now() + 2) == .success)
+
+        let count = coreDataManager.eventsCount()
+        print("Got count:\(count)")
+        XCTAssert(count == desiredCount)
 
 //        var events = [Event]()
 //        for case let report in coreDataManager.reports() {
@@ -57,6 +64,27 @@ class Tests: XCTestCase {
 
         coreDataManager.addEvent(for: "appClose")
         XCTAssert(reportCount() == 2)
+    }
+
+    func testErase() {
+        let coreDataManager = CoreDataManager()
+        let eventCount = { coreDataManager.eventsCount() }
+        XCTAssert(eventCount() == 0)
+        coreDataManager.addEvent(for: "appOpen")
+        coreDataManager.addEvent(for: "appOpen")
+        XCTAssert(eventCount() == 2)
+
+        coreDataManager.eraseAll()
+        XCTAssert(eventCount() == 0)
+        coreDataManager.addEvent(for: "appOpen")
+        coreDataManager.addEvent(for: "appOpen")
+        XCTAssert(eventCount() == 2)
+
+    }
+
+    func testSomething() {
+//        let coredatamanager = CoreDataManager()
+        print(Report.self.debugDescription())
     }
 
 }
