efd1e45d198468d1989c1552a94378e7c6791e36
Akash Desai <kash650@gmail.com>
2018-10-04 11:57:40 -0700

    updated podfile to platform iOS 8

--- Example/Sesame/ViewController.swift
+++ Example/Sesame/ViewController.swift
@@ -30,17 +30,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func didTapSendNotification(_ sender: Any) {
-        UNUserNotificationCenter.current().requestPermission(remoteRegistration: false) { success in
-            guard success == true else {
-                print("Notification permission denied")
-                return
+        if #available(iOS 10.0, *) {
+            UNUserNotificationCenter.current().requestPermission(remoteRegistration: false) { success in
+                guard success == true else {
+                    print("Notification permission denied")
+                    return
+                }
+                UIApplication.shared.sendToBackground()
+                UNUserNotificationCenter.current().scheduleNotification(
+                    identifier: "welcomeScreen",
+                    body: "Welcome to my App!",
+                    time: 2
+                )
             }
-            UIApplication.shared.sendToBackground()
-            UNUserNotificationCenter.current().scheduleNotification(
-                identifier: "welcomeScreen",
-                body: "Welcome to my App!",
-                time: 2
-            )
+        } else {
+            // Fallback on earlier versions
         }
     }
 }
