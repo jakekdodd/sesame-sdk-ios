fbfecfe469106cc6c9536a3017e1e05d3e664912
Akash Desai <kash650@gmail.com>
2018-10-19 17:53:35 -0700

    added Sesame.plist reader

--- Sesame/Classes/Sesame.swift
+++ Sesame/Classes/Sesame.swift
@@ -423,4 +423,35 @@ extension Sesame {
     }
 }
 
+public extension Sesame {
+    public struct PropertyList {
+        public static let file: PropertyList = {
+            guard let path = Bundle.main.path(forResource: "Sesame", ofType: "plist"),
+                let plist = NSDictionary(contentsOfFile: path) as? [String: Any],
+                let sesameProperties = PropertyList(dict: plist)
+                else { fatalError() }
+            return sesameProperties
+        }()
+
+        public let raw: [String: Any]
+        public let appId: String
+        public let auth: String
+        public let versionId: String
+        public let userId: String
+
+        init?(dict: [String: Any]) {
+            guard let appId = dict["appId"] as? String, !appId.isEmpty,
+                let auth = dict["auth"] as? String, !auth.isEmpty,
+                let versionId = dict["versionId"] as? String, !versionId.isEmpty,
+                let userId = dict["userId"] as? String, !userId.isEmpty
+                else { return nil }
+            self.raw = dict
+            self.appId = appId
+            self.auth = auth
+            self.versionId = versionId
+            self.userId = userId
+        }
+    }
+}
+
 //swiftlint:disable:this file_length
